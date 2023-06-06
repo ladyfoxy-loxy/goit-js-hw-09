5dabeb66a221b1db56d9d4130ee3dc174d6973dd
ladyfoxy-loxy <107991649+ladyfoxy-loxy@users.noreply.github.com>
2023-06-06 13:43:23 +0000

    Deploying to gh-pages from @ ladyfoxy-loxy/goit-js-hw-09@9ec36a59ea8e9e00f212d910c535b32197bf5dce 🚀

---
+++ 01-color-switcher.05598226.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(function(){a=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.05598226.js.map
---
+++ 01-color-switcher.81e66801.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.81e66801.js.map
